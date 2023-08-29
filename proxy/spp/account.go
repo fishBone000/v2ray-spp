dbe30ff2fe686e8f6a8bd4b689443b7864d808b8
fishBone000 <FishBoneEK@outlook.com>
2023-08-29 21:16:29 +0800

    Add config structure, client logic and account logic

---
+++ proxy/spp/account.go
@@ -0,0 +1,37 @@
+//go:build !confonly
+// +build !confonly
+
+package spp
+
+import (
+	"github.com/v2fly/v2ray-core/v5/common/protocol"
+)
+
+// MemoryAccount is an in-memory form of spp account.
+type MemoryAccount struct {
+	User     string
+	Password string
+}
+
+// Equals implements protocol.Account.
+func (a *MemoryAccount) Equals(account protocol.Account) bool {
+	sppAccount, ok := account.(*MemoryAccount)
+	if !ok {
+		return false
+	}
+	return a.User == sppAccount.User && a.Password == sppAccount.Password
+}
+
+// AsAccount implements protocol.AsAccount.
+func (a *Account) AsAccount() (protocol.Account, error) {
+  if a.User == "" {
+    return nil, newError("user cannot be empty").AtError()
+  }
+  if a.Password == "" {
+    return nil, newError("password cannot be empty").AtError()
+  }
+	return &MemoryAccount{
+		User:     a.User,
+		Password: a.Password,
+	}, nil
+}
---
+++ proxy/spp/client.go
@@ -0,0 +1,136 @@
+package spp
+
+import (
+	"context"
+	"spp/ray"
+	"time"
+
+	core "github.com/v2fly/v2ray-core/v5"
+	"github.com/v2fly/v2ray-core/v5/app/dns"
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/protocol"
+	"github.com/v2fly/v2ray-core/v5/common/retry"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/common/signal"
+	"github.com/v2fly/v2ray-core/v5/common/task"
+	"github.com/v2fly/v2ray-core/v5/features/policy"
+	"github.com/v2fly/v2ray-core/v5/transport"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+)
+
+type Client struct {
+	serverPicker  protocol.ServerPicker
+	policyManager policy.Manager
+	dns           dns.Client
+}
+
+// NewClient create a new spp client based on the given config.
+func NewClient(ctx context.Context, config *ClientConfig) (*Client, error) {
+	serverList := protocol.NewServerList()
+	for _, rec := range config.Server {
+		s, err := protocol.NewServerSpecFromPB(rec)
+		if err != nil {
+			return nil, newError("failed to get server spec").Base(err)
+		}
+		serverList.AddServer(s)
+	}
+	if serverList.Size() == 0 {
+		return nil, newError("0 target server")
+	}
+
+	v := core.MustFromContext(ctx)
+	c := &Client{
+		serverPicker:  protocol.NewRoundRobinServerPicker(serverList),
+		policyManager: v.GetFeature(policy.ManagerType()).(policy.Manager),
+	}
+
+	return c, nil
+}
+
+// Process implements proxy.Outbound.Process.
+func (c *Client) Process(ctx context.Context, link *transport.Link, dialer internet.Dialer) error {
+	outbound := session.OutboundFromContext(ctx)
+	if outbound == nil || !outbound.Target.IsValid() {
+		return newError("target not specified.")
+	}
+	// Destination of the inner request.
+	destination := outbound.Target
+
+	// Outbound server.
+	var server *protocol.ServerSpec
+	// Outbound server's destination.
+	var dest net.Destination
+	// Connection to the outbound server.
+	var conn internet.Connection
+
+	if err := retry.ExponentialBackoff(5, 100).On(func() error {
+		server = c.serverPicker.PickServer()
+		dest = server.Destination()
+		rawConn, err := dialer.Dial(ctx, dest)
+		if err != nil {
+			return err
+		}
+		conn = rawConn
+
+		return nil
+	}); err != nil {
+		return newError("failed to find an available destination").Base(err)
+	}
+
+	defer func() {
+		if err := conn.Close(); err != nil {
+			newError("failed to close connection").Base(err).WriteToLog(session.ExportIDToError(ctx))
+		}
+	}()
+
+	p := c.policyManager.ForLevel(0)
+
+	user := server.PickUser()
+	var username string
+	var password string
+	if user != nil {
+		p = c.policyManager.ForLevel(user.Level)
+		memoryAccount := user.Account.(*MemoryAccount)
+		username = memoryAccount.User
+		password = memoryAccount.Password
+	} else {
+		username = ""
+		password = ""
+	}
+
+	if err := conn.SetDeadline(time.Now().Add(p.Timeouts.Handshake)); err != nil {
+		newError("failed to set deadline for handshake").Base(err).WriteToLog(session.ExportIDToError(ctx))
+	}
+	ray, err := ray.NewFromConn(conn, []byte(username), []byte(password))
+	if err != nil {
+		return newError("handshake failed").Base(err).AtError()
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	timer := signal.CancelAfterInactivity(ctx, cancel, p.Timeouts.ConnectionIdle)
+
+	requestFunc := func() error {
+		defer timer.SetTimeout(p.Timeouts.DownlinkOnly)
+    // TODO Add error handling for wpacket recovery
+		return buf.Copy(link.Reader, buf.NewWriter(ray), buf.UpdateActivity(timer))
+	}
+  responseFunc := func() error {
+    defer timer.SetTimeout(p.Timeouts.UplinkOnly)
+    return buf.Copy(buf.NewReader(ray), link.Writer, buf.UpdateActivity(timer))
+  }
+
+  responseDonePost := task.OnSuccess(responseFunc, task.Close(link.Writer))
+  if err := task.Run(ctx, requestFunc, responseDonePost); err != nil {
+    return newError("connection ends").Base(err)
+  }
+
+  return nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return NewClient(ctx, config.(*ClientConfig))
+	}))
+}
